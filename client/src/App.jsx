ecd337bef7e2a46357ce7d8b370efeaeb13587e0
Abhinav <abhinav280601@gmail.com>
2023-02-26 15:18:09 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+import { Sidebar, Navbar } from './components';
+import { CampaignDetails, CreateCampaign, Home, Profile } from './pages';
+
+const App = () => {
+  return (
+    <div className="relative sm:-8 p-4 bg-[#13131a] min-h-screen flex flex-row">
+      <div className="sm:flex hidden mr-10 relative">
+        <Sidebar />
+      </div>
+
+      <div className="flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5">
+        <Navbar />
+        <div >
+            <center><img
+              style={{ minWidth: "50%"}}
+              className="d-block w-100 lazyload mb-10px"
+              src="https://voorent-new.s3.ap-south-1.amazonaws.com/charity_logo+(2).jpg"
+              alt="Second slide"
+            /></center><br></br>
+        </div>
+
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/create-campaign" element={<CreateCampaign />} />
+          <Route path="/campaign-details/:id" element={<CampaignDetails />} />
+        </Routes>
+      </div>
+
+     
+      
+    </div>
+  )
+}
+
+export default App
